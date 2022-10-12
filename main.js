e82176da0db5191c7d0e2f0ddbac158b2607de64
babbu47 <111335500+babbu47@users.noreply.github.com>
2022-10-12 10:18:42 +0530

    refactor:text changes

---
+++ main.js
@@ -0,0 +1,8 @@
+var date=document.querySelector("#dating")
+var write=document.querySelector("#output")
+var check=document.querySelector("#buto")
+check.addEventListener("click",function S(){
+write.innerText="hi"
+
+
+})
